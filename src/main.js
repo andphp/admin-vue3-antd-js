da8ebdd32e476051e74cbcdfc644f40845e5f847
daxiong <417170808@qq.com>
2021-01-02 16:04:05 +0800

    init vue3

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/* jshint esversion: 6 */
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
