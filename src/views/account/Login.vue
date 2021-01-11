382a01fbb250d028dfe2ac92e79d75af84bbd61b
unknown <you@example.com>
2021-01-11 21:02:41 +0800

    add icon

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -13,7 +13,8 @@
             headStyle="text-align: center;font-size: 24px;color: #1890ff;"
             :loading="signUpMode"
           >
-            <a-form name="custom-validation" class="sign-in-form" @submit="loginSubmit">
+            <a-form name="custom-validation" class="sign-in-form" :model="user" :rules="formRule" ref="formRef"
+                    @submit="loginSubmit">
               <a-form-item>
                 <a-input
                   size="large"
@@ -21,7 +22,9 @@
                   placeholder="账户: admin"
                   v-model:value="user.username"
                 >
-                  <svg-icon icon-name="user" style="width:20px; height:20px;" />
+                  <template #prefix>
+                    <UserOutlined style="color:#1890ff" />
+                  </template>
                 </a-input>
               </a-form-item>
               <a-form-item>
@@ -32,12 +35,14 @@
                     placeholder="密码: admin or ant.design"
                     v-model:value="user.password"
                   >
-                    <svg-icon icon-name="locked"></svg-icon>
+                    <template #prefix>
+                      <LockOutlined style="color:#1890ff" />
+                    </template>
                   </a-input-password>
                 </div>
               </a-form-item>
               <a-form-item :wrapper-col="{ span: 24 }">
-                <a-button type="primary" html-type="submit" class="login-button" block>
+                <a-button size="large" type="primary" html-type="submit" class="login-button" block>
                   {{ $t("login.btn_title") }}
                 </a-button>
               </a-form-item>
@@ -50,7 +55,7 @@
     <div class="panels-container">
       <div class="panel left-panel">
         <div class="content">
-          <h3>admin-vue3-antd</h3>
+          <h1>Admin-Vue3-Antd</h1>
           <p>牛逼plus 江北区最具影响力的 Web 后台管理系统</p>
         </div>
         <img src="@/assets/login/table_bg.png" class="image" alt="" />
@@ -58,7 +63,7 @@
 
       <div class="panel right-panel">
         <div class="content">
-          <h3>以人为镜,可明得失, 以代码为镜,可通逻辑!</h3>
+          <h1>以人为镜,可明得失, 以代码为镜,可通逻辑!</h1>
           <p>学习编程,让你的生活更有趣</p>
           <button @click="signUpMode = !signUpMode" class="btn transparent">
             取消
@@ -71,8 +76,9 @@
 </template>
 <script>
 import { ref, reactive } from "vue";
-import { Form, Input, Card, Button } from "ant-design-vue";
-import SvgIcon from "../../components/Svgicon";
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+import { Form, Input, Card, Button, Message } from "ant-design-vue";
+import { useRouter } from "vue-router";
 
 export default {
   name: "Login",
@@ -83,21 +89,44 @@ export default {
     AInputPassword: Input.Password,
     ACard: Card,
     AButton: Button,
-    SvgIcon: SvgIcon
+    UserOutlined: UserOutlined,
+    LockOutlined: LockOutlined
   },
   setup() {
+    const router = useRouter();
+    const formRef = ref(null);
     let signUpMode = ref(false);
     let user = reactive({
       username: "admin",
       password: "123456"
     });
 
-    function loginSubmit() {
-      console.log(signUpMode);
-      signUpMode.value = !signUpMode.value;
-    }
+    //验证规则
+    let rules = reactive({
+      formRule: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [
+          { required: true, min: 6, message: "密码最少六位", trigger: "blur" }
+        ]
+      }
+    });
+    const loginSubmit = () => {
+      formRef.value.validate().then(() => {
+        console.log(signUpMode);
+        signUpMode.value = !signUpMode.value;
+        setTimeout(function() {
+          router.push({ path: "/" });
+        }, 3000);
+      })
+        .catch(() => {
+          //验证失败提示错误信息
+          Message.error("用户名或密码未填");
+        });
+    };
 
-    return { signUpMode, loginSubmit, user };
+    return { signUpMode, loginSubmit, user, formRef };
   }
 };
 </script>
@@ -123,7 +152,7 @@ export default {
   border-radius: 20px 20px 20px 20px;
   background: rgba(250, 250, 250, 0.7);
   box-shadow: 0px 2px 14px 6px rgba(24, 144, 255, 0.2),
-  -11px -4px 9px -16px rgba(24, 144, 255, 0.2);
+    -11px -4px 9px -16px rgba(24, 144, 255, 0.2);
   background: transparent;
 }
 
